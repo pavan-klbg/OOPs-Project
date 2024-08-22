86d8fce7b134f12faf70d2f50973f40ddac8c2db
pavan-klbg <allurpavan111@gmail.com>
2024-08-22 17:46:06 +0530

    added getters and setters, constructors

--- CarRentalProject/src/Customer.java
+++ CarRentalProject/src/Customer.java
@@ -1,2 +1,44 @@
-package PACKAGE_NAME;public class Customer {
+public class Customer {
+    private int customerId;
+    private String customerName;
+    private String customerAddress;
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerAddress() {
+        return customerAddress;
+    }
+
+    public void setCustomerAddress(String customerAddress) {
+        this.customerAddress = customerAddress;
+    }
+
+    public Customer(int customerId, String customerName, String customerAddress) {
+        this.customerId = customerId;
+        this.customerName = customerName;
+        this.customerAddress = customerAddress;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "customerId=" + customerId +
+                ", customerName='" + customerName + '\'' +
+                ", customerAddress='" + customerAddress + '\'' +
+                '}';
+    }
 }
