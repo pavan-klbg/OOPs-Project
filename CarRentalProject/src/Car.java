51536a002277cc240ca0f13cd18ee139ac21cd85
pavan-klbg <allurpavan111@gmail.com>
2024-08-22 17:42:56 +0530

    car class update with getters and setter

--- CarRentalProject/src/Car.java
+++ CarRentalProject/src/Car.java
@@ -1,2 +1,58 @@
-package PACKAGE_NAME;public class Car {
+public class Car {
+    private int carId;
+    private String carType;
+
+    private String carName;
+
+    private boolean isAvailable;
+
+
+    public int getCarId() {
+        return carId;
+    }
+
+    public void setCarId(int carId) {
+        this.carId = carId;
+    }
+
+    public String getCarType() {
+        return carType;
+    }
+
+    public void setCarType(String carType) {
+        this.carType = carType;
+    }
+
+    public String getCarName() {
+        return carName;
+    }
+
+    public void setCarName(String carName) {
+        this.carName = carName;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(boolean available) {
+        isAvailable = available;
+    }
+
+    public Car(int carId, String carType, String carName, boolean isAvailable) {
+        this.carId = carId;
+        this.carType = carType;
+        this.carName = carName;
+        this.isAvailable = isAvailable;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "carId=" + carId +
+                ", carType='" + carType + '\'' +
+                ", carName='" + carName + '\'' +
+                ", isAvailable=" + isAvailable +
+                '}';
+    }
 }
